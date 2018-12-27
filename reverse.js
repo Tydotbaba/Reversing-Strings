3d88ff2abaacd6dc63ca19224f434c00f5873c5d
Temitayo Oyedokun <oyes2011@gmail.com>
2018-12-27 15:42:17 +0100

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,8 +1,11 @@
 /*
   This function accepts a string as an 
   argument and returns it reversed.
+  there are two ways i've implemented this
 */
 
+
+// first implementation; using built-in functions
 function reverseString(str) {
   ///let strs = str.split("");
   ///var arr = strs.reverse();
@@ -10,6 +13,15 @@ function reverseString(str) {
   return str.split("").reverse().join("");
 }
 
+// second implementation; using array
+function reverseByArray(str) {
+  var s = "";
+  for(let i = str.length - 1; i >= 0; i--){
+    s += str[i];
+  }
+  return s;
+}
+
 console.log(reverseString("olleh"));
-console.log(reverseString("oyaT"));
+console.log(reverseByArray("oyaT"));
 console.log(reverseString("adohR"));
